b4cc96c209b2dff11d4c249b1dd8aa8d80abbf13
Horuss <kuba.horuss@gmail.com>
2016-08-16 01:45:55 +0200

    js refactoring

--- src/main/resources/pl/horuss/bbplay/web/d3/diagram_connector.js
+++ src/main/resources/pl/horuss/bbplay/web/d3/diagram_connector.js
@@ -4,9 +4,8 @@ window.pl_horuss_bbplay_web_d3_Diagram = function() {
 			"width", 500).attr("height", 500);
 	var steps;
 	
-	this.init = function(stepsStr) {
-		steps = eval(stepsStr);
-		steps[0].entites.forEach(function(entity, entityNo) {
+	function drawStep(step) {
+		step.entites.forEach(function(entity, entityNo) {
 			var g = diagramFrame.append("svg")
 				.attr("class", "node")
 				.attr("id", "se" + entity.id);
@@ -27,34 +26,19 @@ window.pl_horuss_bbplay_web_d3_Diagram = function() {
 				.text(entity.label)
 			g.attr("x", entity.x).attr("y", entity.y)
 		});
+	}
+	
+	this.init = function(stepsStr) {
+		steps = eval(stepsStr);
+		drawStep(steps[0]);
 	},
 
 	this.play = function(speed, delay) {
-		steps.shift();
 		steps.forEach(function(step, stepNo) {
-			step.entites.forEach(function(entity, entityNo) {
+			if (stepNo != 0) step.entites.forEach(function(entity, entityNo) {
 				var selEnt = diagramFrame.select("#se" + entity.id);
 				if (selEnt.empty()) {
-					var g = diagramFrame.append("svg")
-						.attr("class", "node")
-						.attr("id", "se" + entity.id);
-					g.append("circle")
-						.attr("r", "10")
-						.attr("cx", 12)
-						.attr("cy", 12)
-						.attr("stroke", "#D68400")
-						.attr("stroke-width", "2")
-						.style("fill", "#FFAE00");
-					g.append("text")
-						.attr("dy", "15")
-						.attr("dx", 10)
-						.attr("class", "label")
-						.attr("stroke-width", "1")
-						.attr("fill", "#000")
-						.style("font-size", '10px')
-						.text(entity.label)
-					g.attr("x", entity.x).attr("y", entity.y)
-
+					drawStep(step)
 				} else {
 					selEnt.transition()
 						.delay(delay * 1000)
