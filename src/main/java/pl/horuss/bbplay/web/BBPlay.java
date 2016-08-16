01052076dc46b6866ba3ff45de1f26e17ed5ce21
Horuss <kuba.horuss@gmail.com>
2016-08-17 01:03:20 +0200

    build fix

--- src/main/java/pl/horuss/bbplay/web/BBPlay.java
+++ src/main/java/pl/horuss/bbplay/web/BBPlay.java
@@ -1,5 +1,13 @@
 package pl.horuss.bbplay.web;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
@@ -9,6 +17,19 @@ public class BBPlay {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BBPlay.class, args);
+	}
+
+	@Autowired
+	private ServletContext servletContext;
+
+	@PostConstruct
+	public void init() throws IOException {
+		String root = servletContext.getRealPath("/");
+		if (root != null && Files.isDirectory(Paths.get(root))) {
+			// workaround for https://dev.vaadin.com/ticket/18463
+			Files.createDirectories(Paths.get(servletContext
+					.getRealPath("/VAADIN/themes/bbplay-theme")));
+		}
 	}
 
 }
--- src/main/java/pl/horuss/bbplay/web/parts/Footer.java
+++ src/main/java/pl/horuss/bbplay/web/parts/Footer.java
@@ -12,7 +12,9 @@ public class Footer extends VerticalLayout {
 		addStyleName("footer");
 
 		final Label content = new Label(
-				"<strong>BBPlay</strong> \u00a9 2016 by <a href=\"http://www.horuss.pl\">Horuss</a> | Please report <a href=\"https://github.com/Horuss/bbplay/issues\">bugs & features</a>");
+				"<strong>BBPlay</strong> (v. "
+						+ Footer.class.getPackage().getImplementationVersion()
+						+ ") \u00a9 2016 by <a href=\"http://www.horuss.pl\">Horuss</a> | Please report <a href=\"https://github.com/Horuss/bbplay/issues\">bugs & features</a>");
 		content.setContentMode(ContentMode.HTML);
 		content.setWidthUndefined();
 		addComponent(content);
